d70e9cdfa24502fcdcb6395e65c6496b58e8d0f6
tokkun0916 <naokitokuhara@gmail.com>
2026-06-15 03:19:01 +0900

    初期セットアップ

---
+++ Assets/Script/Player/HogePlayerMover.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// WASDで移動するシンプルなプレイヤー
+/// </summary>
+public class HogePlayerMover : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5f;
+
+    private void Update()
+    {
+        Vector2 input = Vector2.zero;
+
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.aKey.isPressed) input.x -= 1;
+            if (Keyboard.current.dKey.isPressed) input.x += 1;
+            if (Keyboard.current.sKey.isPressed) input.y -= 1;
+            if (Keyboard.current.wKey.isPressed) input.y += 1;
+        }
+
+        Vector3 move =
+            new Vector3(input.x, 0f, input.y).normalized;
+
+        transform.position +=
+            move *
+            _moveSpeed *
+            Time.deltaTime;
+    }
+}
