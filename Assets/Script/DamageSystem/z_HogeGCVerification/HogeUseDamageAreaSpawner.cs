62bf477379a2709990b7942b88afa6cb5fa357f2
tokkun0916 <naokitokuhara@gmail.com>
2026-07-06 00:23:06 +0900

    コード修正：DamageAreaのランダム要素を削減
    検証のためにランダムな要素をできるだけ固定化した

--- Assets/Script/DamageSystem/z_HogeGCVerification/HogeUseDamageAreaSpawner.cs
+++ Assets/Script/DamageSystem/z_HogeGCVerification/HogeUseDamageAreaSpawner.cs
@@ -8,8 +8,12 @@ using UnityEngine;
 public class HogeUseDamageAreaSpawner : MonoBehaviour
 {
     [SerializeField] private DamageAreaSpwaner _damageAreaSpawner;
-    [SerializeField] private int _spawnNumberPerSecond = 20;
+    [SerializeField] private int _spawnNumberPerSecond = 10000;
+    [SerializeField] private int _targetSpawnCount = 100000;
+    private int _spawnCount = 0;
     private float _spawnTimer;
+
+    private int _spawnType = 1;
 
     void Update()
     {
@@ -17,60 +21,66 @@ public class HogeUseDamageAreaSpawner : MonoBehaviour
 
         while (_spawnTimer >= 1f)
         {
-            DamageAreaData data = RandomSetDamageAreaData();
+            DamageAreaData data = SetDamageAreaData();
             _damageAreaSpawner.Spawn(data);
 
             _spawnTimer -= 1f;
+
+            _spawnCount++;
+        }
+        if (_spawnCount >= _targetSpawnCount)
+        {
+            EndTest();
         }
     }
 
-    private DamageAreaData RandomSetDamageAreaData()
+    private void EndTest()
     {
-        // ランダムで矩形か円形のダメージエリアを生成する
-        switch (UnityEngine.Random.Range(0, 2))
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private DamageAreaData SetDamageAreaData()
+    {
+        // 検証用に同一の結果となるようにダメージエリアの形状を交互に切り替える
+        _spawnType *= -1;
+        switch (_spawnType)
         {
-            case 0:
+            case -1:
                 return new DamageAreaData(
                     new DamageAreaRectData(
-                        frontCenterPos: new Vector3(
-                            Random.Range(-5f, 5f),
-                            0f,
-                            Random.Range(-5f, 5f)),
-                        size: new Vector3(
-                            Random.Range(1f, 5f),
-                            Random.Range(1f, 5f),
-                            Random.Range(1f, 5f)
-                        )
+                        frontCenterPos: Vector3.zero,
+                        size:           Vector3.one
                     ),
                     new DamageAreaAttackData(),
                     new DamageAreaTimeData(
-                        spawnTime:       Random.Range(0.5f, 2f),
-                        attackWaitTime:  Random.Range(0.5f, 2f),
-                        attackTime:      Random.Range(0.5f, 2f),
-                        fadeOutTime:     Random.Range(0.5f, 2f)
+                        spawnTime:      1f,
+                        attackWaitTime: 1f,
+                        attackTime:     1f,
+                        fadeOutTime:    1f
                     )
                 );
             case 1:
                 return new DamageAreaData(
                     new DamageAreaCircleData(
-                        centerPosition: new Vector3(
-                            Random.Range(-5f, 5f),
-                            0f,
-                            Random.Range(-5f, 5f)),
-                        height: Random.Range(1f, 5f),
-                        radius: Random.Range(1f, 5f),
-                        angle: 360f
+                        centerPosition: Vector3.zero,
+                        height: 1f,
+                        radius: 1f,
+                        angle:  360f
                     ),
                     new DamageAreaAttackData(),
                     new DamageAreaTimeData(
-                        spawnTime:       Random.Range(0.5f, 2f),
-                        attackWaitTime:  Random.Range(0.5f, 2f),
-                        attackTime:      Random.Range(0.5f, 2f),
-                        fadeOutTime:     Random.Range(0.5f, 2f)
+                        spawnTime:       1f,
+                        attackWaitTime:  1f,
+                        attackTime:      1f,
+                        fadeOutTime:     1f
                     )
                 );
             default:
-                throw new System.Exception("Invalid random value for damage area type.");
+                throw new System.Exception("Invalid spawn type for damage area.");
         }
     }
 }
