b1bda3d2b0a23a1ecf06d1b970e0e822d8101e00
tokkun0916 <naokitokuhara@gmail.com>
2026-07-08 20:25:40 +0900

    コード修正：UniRxで通知するイベントデータをクラスから構造体へ
    イベントデータをクラスで作成するとGC.Allocの原因になるので
    ストラクトに変更する

--- Assets/Script/DamageSystem/Data/State/DamageAreaStateChanged.cs
+++ Assets/Script/DamageSystem/Data/State/DamageAreaStateChanged.cs
@@ -2,17 +2,17 @@
 /// ダメージエリアの状態変更時に通知されるイベントデータ。
 /// 状態遷移後の状態と、その時点のダメージエリア情報を保持する。
 /// </summary>
-public class DamageAreaStateChanged
+public readonly struct DamageAreaStateChanged
 {
     /// <summary>
     /// 遷移後のダメージエリアの状態
     /// </summary>
-    public DamageAreaState State { get; private set; }
+    public DamageAreaState State { get;}
 
     /// <summary>
     /// 状態変更時点のダメージエリア情報
     /// </summary>
-    public DamageAreaData Data { get; private set; }
+    public DamageAreaData Data { get;}
 
     public DamageAreaStateChanged(DamageAreaState state, DamageAreaData data)
     {
